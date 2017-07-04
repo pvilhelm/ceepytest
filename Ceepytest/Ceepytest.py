4d0535be9ca232c421362192cf97f658a98d23cf
Petter <tomner@kth.se>
2017-07-04 09:51:36 +0200

    ADding stuff

--- Ceepytest/Ceepytest.py
+++ Ceepytest/Ceepytest.py
@@ -8,12 +8,6 @@ import subprocess
 def main():
     ans = subprocess.run("echo hej",shell=True,stdout=subprocess.PIPE)
 
-
-
-
-
-
-
 if __name__ == "__main__":
     main()
 
--- Ceepytest/cfile.py
+++ Ceepytest/cfile.py
@@ -1,6 +1,7 @@
 
 import sys
 import os
+import re
 
 class cfile:
 
@@ -8,11 +9,28 @@ class cfile:
     def __init__(self, file_path):
         self.file_path = file_path
         f = open(file_path) 
-        self.str = f.readlines()
+        self.list_of_lines = f.readlines()
+
+    def first_pass(self):
+        i = 0
+        for line in self.list_of_lines:
+            if(re.match(r"\w*\/*#")):
+                #find matching end
+                j = i+1
+                for line in self.list_of_lines[i:]:
+                    if(re.match(r"\w*\*\/")):
+                        a=2
+                    j = j+1
+                i=i+1
+            
 
 def test():
     cf = cfile("test_A.c")
+    print(cf.str)
+
+    print("cfile.py self-test passed")
+    return True
 
 
 if __name__ == "__main__":
-    test()
+    assert(test())
