398d59c09ac366477eb6afd1c109726cecb5d19c
Petter Tomner <petter.vilhelm@gmail.com>
2017-07-04 23:52:22 +0200

    Beginning to look like something

--- Ceepytest/cfile.py
+++ Ceepytest/cfile.py
@@ -12,34 +12,72 @@ class cfile:
         f = open(file_path) 
         self.full_str = f.read()
 
+        self.dict_local_vars = {"C":"//", "C0":"/*", "C1":"*/"}
+        self.dict_global_vars = {}
         # Parse Ceepytest code snipps
         self.first_pass()
         # Evaluate all those snipps
         self.sec_pass()
+        
 
     def first_pass(self):
         """Parse a c-file for Ceepytest patterns and store the corrensponding strings."""
 
-        self.list_of_lines = []
-        self.list_of_py_str_span_tuples = []
-        self.list_of_assign_str_span_tuples = []
-        self.list_of_assert_str_span_tuples = []
         self.dict_strs = {} #dict of dict of dict of segment of text
         #first dict: key
         
         # Python inline code
         #find all patterns /*# ... */ 
-        for m in re.finditer(r"\/\*#(.*?)(\*\/)",self.full_str, re.DOTALL | re.MULTILINE):
-            tmp_str = m[1].strip() 
-            self.dict_strs[m.start(1)] = {"type":"py_inline","start":m.start(1),"end":m.end(1),"after":m.end(2),"str":tmp_str}
-        #find all patterns //# ... \n
-        for m in re.finditer(r"\/\/#(.*?)(\n)",self.full_str, re.DOTALL | re.MULTILINE):
+        for m in re.finditer(r"\/\*#(.*?)(#\*\/)",self.full_str, re.DOTALL | re.MULTILINE):
             tmp_str = m[1].strip()
             self.dict_strs[m.start(1)] = {"type":"py_inline","start":m.start(1),"end":m.end(1),"after":m.end(2),"str":tmp_str}
+            if(m[0].find('\n')!=-1):    #code segment spans multiple lines so add new line to the output later
+                self.dict_strs[m.start(1)]["inline"] = True
+            else:
+                self.dict_strs[m.start(1)]["inline"] = False
 
+        #find all patterns //# ... \n
+        for m in re.finditer(r"\/\/#(.*?)(\n)",self.full_str, re.DOTALL | re.MULTILINE):
+            tmp_str = "\n"+m[1].strip() 
+            self.dict_strs[m.start(1)] = {"type":"py_inline","start":m.start(1),"end":m.end(1),"after":m.end(2),"str":tmp_str}
+
+        
+
+    def sec_pass(self):
+        """Construct c-code for inline python """
+        
+        self.c_str_additions = {}
+
+        #iterate over all dict entrys and call the correct function if the entry
+        #needs to be run before other code (i.e inline python code that can write
+        #more Ceepytest code) /*# ... #*/
+        for k in sorted(self.dict_strs.keys()):
+            d = self.dict_strs[k]
+            type = d["type"]
+            if(type == "py_inline"):
+                self.c_str_additions[d["after"]] = ("\n" if d["inline"] else " ") + self.py_inline(d["str"])
+
+        #add the python inline code output to the c-file
+        self.out_c_str = ""
+
+        pos_to_add_to = sorted(self.c_str_additions.keys())
+        
+        #stich the strings together
+        old_pos = 0
+        for pos in pos_to_add_to:
+            self.out_c_str += self.full_str[old_pos:pos]
+            #self.out_c_str += "/* Start generated code from pos: %s */\n" % pos
+            self.out_c_str += self.c_str_additions[pos]
+            #self.out_c_str += "/* End generated code from pos: %s */\n" % pos
+            old_pos = pos
+            print(self.full_str+"\n")
+        
+        #if(old_pos<len(self.out_c_str)): #uneccesary?
+        self.out_c_str += self.full_str[old_pos:]
+        
         # Assignments
         #find all patterns /*! ... */
-        for m in re.finditer(r"\/\*!(.*?)(\*\/)",self.full_str, re.DOTALL | re.MULTILINE):
+        for m in re.finditer(r"\/\*!(.*?)(!\*\/)",self.full_str, re.DOTALL | re.MULTILINE):
             tmp_str = m[1].strip()
             self.dict_strs[m.start(1)] = {"type":"assigns","start":m.start(1),"end":m.end(1),"after":m.end(2),"str":tmp_str}
         #find all patterns //!
@@ -49,42 +87,41 @@ class cfile:
 
         # Asserts
         #find all patterns /*? ... */
-        for m in re.finditer(r"\/\*\?(.*?)(\*\/)",self.full_str, re.DOTALL | re.MULTILINE):
+        for m in re.finditer(r"\/\*\?(.*?)(\?\*\/)",self.full_str, re.DOTALL | re.MULTILINE):
             tmp_str = m[1].strip()
             self.dict_strs[m.start(1)] = {"type":"asserts","start":m.start(1),"end":m.end(1),"after":m.end(2),"str":tmp_str}
         #find all patterns //?
         for m in re.finditer(r"\/\/\?(.*?)(\n)",self.full_str, re.DOTALL | re.MULTILINE):
             tmp_str = m[1].strip()
             self.dict_strs[m.start(1)] = {"type":"asserts","start":m.start(1),"end":m.end(1),"after":m.end(2),"str":tmp_str}
-        for k,v in self.dict_strs.items():
-            print("key:"+str(k))
-            for k,v in v.items():
-                print("\t"+str(k)+":"+str(v))
+        #for k,v in self.dict_strs.items():
+        #    print("key:"+str(k))
+        #    for k,v in v.items():
+        #        print("\t"+str(k)+":"+str(v))
 
-    def sec_pass(self):
-        """ Construct c-code for all Ceepytest strings found in the file"""
-
-        #iterate over all dict entrys and call the correct function
-        for k in sorted(self.dict_strs.keys()):
-            d = self.dict_strs[k]
-            type = d["type"]
-            if(type == "py_inline"):
-                py_inline(d["str"])
+    def third_pass(self):
+        NOP
 
     def py_inline(self, str):    
         old_stdout = sys.stdout
         my_stdout = sys.stdout = StringIO()
-        eval()
-        sys.stdout = self.old_stdout
-
-        print(my_stdout.getvalue())
+        exec(str,self.dict_global_vars,self.dict_local_vars)
+        sys.stdout = old_stdout
+        ret_str = my_stdout.getvalue()
         my_stdout.close()
+        return ret_str
 
+    def assigns(self, str):
+        ret_str = ""
+        self.dict_assigns = {}
+
+
+
+        return ret_str
 
 def test():
     cf = cfile("test_A.c")
-    cf.first_pass()
-
+    print(cf.out_c_str)
     print("cfile.py self-test passed")
     return True
 
--- Ceepytest/test_A.c
+++ Ceepytest/test_A.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /*#
-print("Hello from Ceepytest running test_A.c!")
-*/
+print(r'printf("Hello from Ceepytest running test_A.c!"')
+print(C0 + " A c-comment ")
+print("Ceepytest captures stdout and writes it underneath the block" + C1)
+#*/
  
-/*# print("This can be done too")*/ int a; /*# print("So can this")*/
+/*# print("int") #*/ a = /*# print( 2+2, C+"So can this") #*/
 
 void a_func(int a){
 	//@reach	
@@ -22,8 +24,8 @@ int b_func(float b){
 /*!
 x = 1,2,3,4,5,6,7,8,9
 ans = 2,3,4,5,6,7,8,10
-*/
+!*/
 
 /*?
 b_func(@x) == @ans
-*/
+?*/
